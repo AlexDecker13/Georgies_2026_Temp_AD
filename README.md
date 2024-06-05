1a7436953b2d1f3afffe6849c43af0e29b73dc13
Miika Nissi <miika@miikanissi.com>
2024-06-05 00:30:43 -0400

    [MIG] customize_pos_reports: Migrate to 17

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Available addons
 ----------------
 addon | version | maintainers | summary
 --- | --- | --- | ---
+[customize_pos_reports](customize_pos_reports/) | 17.0.1.0.0 | [![miikanissi](https://github.com/miikanissi.png?size=30px)](https://github.com/miikanissi) | Customized Receipt of Point Of Sales
 [sale_order_qty_stock_move](sale_order_qty_stock_move/) | 17.0.1.0.0 | [![miikanissi](https://github.com/miikanissi.png?size=30px)](https://github.com/miikanissi) | Increase or Decrease in stock move quantity per sale order
 
 [//]: # (end addons)
---
+++ customize_pos_reports/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "POS Custom Receipt",
+    "summary": "Customized Receipt of Point Of Sales",
+    "version": "17.0.1.0.0",
+    "category": "Sales/Point of Sale",
+    "website": "https://github.com/rtonneso/https-github.com-rtonneso-RJ/",
+    "author": "Georgie's Ceramic & Clay Co., Konsultoo Software Consulting",
+    "maintainers": ["miikanissi"],
+    "installable": True,
+    "application": False,
+    "depends": ["point_of_sale"],
+    "data": [],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "customize_pos_reports/static/src/**/*",
+        ],
+    },
+}
