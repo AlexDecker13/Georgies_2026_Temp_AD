8cdfdb0288ba4ed412287e016de617dbdb8df451
Miika Nissi <miika@miikanissi.com>
2024-06-04 23:49:48 -0400

    [IMP] sale_order_qty_stock_move: pre-commit fixes

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Odoo Addons for Georgie's Ceramic and Clay Co.
 <!-- prettier-ignore-start -->
 
 [//]: # (addons)
+
+Unported addons
+---------------
+addon | version | maintainers | summary
+--- | --- | --- | ---
+[sale_order_qty_stock_move](sale_order_qty_stock_move/) | 14.0.0.1 (unported) |  | To Increase or Decrease in sock move quantity as per sale order
+
 [//]: # (end addons)
 
 <!-- prettier-ignore-end -->
---
+++ sale_order_qty_stock_move/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sale Order Quantity to Stock Move",
+    "version": "14.0.0.1",
+    "category": "Uncategorized",
+    "summary": "To Increase or Decrease in sock move quantity as per sale order",
+    "author": "Konsultoo Software Consulting",
+    "website": "https://github.com/rtonneso/https-github.com-rtonneso-RJ/",
+    "description": "To Increase or Decrease in sock move quantity as per sale order",
+    "depends": ["sale_stock"],
+    "data": [],
+    "demo": [],
+    "installable": False,
+}
