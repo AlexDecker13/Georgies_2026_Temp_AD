a96106745fa3a350e3b5a59918ff065e50ea99e1
Miika Nissi <miika@miikanissi.com>
2024-06-05 00:31:31 -0400

    [IMP] migration script remove customize_pos_reports

--- sale_order_qty_stock_move/migrations/17.0.1.0.0/end-migration.py
+++ sale_order_qty_stock_move/migrations/17.0.1.0.0/end-migration.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     _logger.info(
-        "Removing old modules that are no longer needed:\n - auto_backup\n - customize_pos_reports\n - interest_on_overdue_invoices\n - theme_georgies"
+        "Removing old modules that are no longer needed:\n - auto_backup\n - interest_on_overdue_invoices\n - theme_georgies"
     )
     util.remove_module(cr, "auto_backup")
     util.remove_module(cr, "interest_on_overdue_invoices")
