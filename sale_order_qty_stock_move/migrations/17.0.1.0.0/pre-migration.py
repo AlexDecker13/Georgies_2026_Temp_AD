e226b666e9ca1bd54616587f36a6ec43651aa723
Miika Nissi <miika@miikanissi.com>
2024-06-08 00:31:43 -0400

    [IMP] Disable broken studio views

---
+++ sale_order_qty_stock_move/migrations/17.0.1.0.0/pre-migration.py
@@ -0,0 +1,96 @@
+import logging
+
+from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+
+def disable_record(cr, xml_id):
+    """
+    This function disables a record in the database.
+
+    Parameters:
+    cr (Cursor): The database cursor, used for database operations.
+    xml_id (str): The XML ID of the record to be disabled.
+
+    Returns:
+    None
+    """
+    record = util.ref(
+        cr,
+        xml_id,
+    )
+    if record:
+        util.env(cr)["ir.ui.view"].browse(record).write({"active": False})
+
+
+def migrate(cr, version):
+    _logger.info("Disabling broken studio views...")
+    disable_record(
+        cr,
+        "studio_customization.default_graph_view_f_5767c0ff-731c-413a-adc2-3cd4a8a90831",
+    )
+    disable_record(
+        cr,
+        "studio_customization.odoo_studio_default__73689efd-c3ca-4fe9-a4bc-a26b220e8187",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_2de82db8-341c-4e3d-b4c6-780cee3dc769",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_c02ffc13-4be2-4884-a4ae-a062713cc421",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_e408e6d6-bed9-4582-ae96-acf5e79c9a59",
+    )
+    disable_record(
+        cr,
+        "studio_customization.odoo_studio_default__d19b491d-4267-4894-a9c9-d28e8fbb680b",
+    )
+    disable_record(
+        cr,
+        "studio_customization.odoo_studio_default__0cf9a039-e01a-4d01-a6f6-4ec045f8e9a8",
+    )
+    disable_record(
+        cr,
+        "studio_customization.odoo_studio_default__6ce488e0-a379-4400-9366-5412193cf23a",
+    )
+    disable_record(
+        cr,
+        "studio_customization.odoo_studio_default__4230ab7a-2cbd-46cc-9825-f8dd0489d58e",
+    )
+    disable_record(
+        cr,
+        "studio_customization.odoo_studio_default__0cd4bf5b-83de-4cfe-b129-5af96acfab2f",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_088c8673-dd2f-4d09-a9f3-e73dfbe527e1",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_5eb8a9fe-d8c1-4a26-88ad-33a2aa7bfc2f",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_024fe98e-b2dd-4d60-88c0-3c5c573f246c",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_a2fd9ee1-3796-4747-b3c9-b0d2a8db8e10",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_2bbab653-780c-47e1-b54c-196367e28ea6",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_f44c36f8-84de-46da-bc24-c84201beae96",
+    )
+    disable_record(
+        cr,
+        "studio_customization.default_tree_view_fo_d324d9f3-ed8d-40b6-bc9c-508ddc41bc12",
+    )
